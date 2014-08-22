ad10277cdbfd08ee4ed5db2fa0aadbcaccea4a5b
Giles Lavelle <lavelle@chromium.org>
2014-08-22 11:11:07 -0700

    latest attempt at validation

--- s3fs/ui/auth.js
+++ s3fs/ui/auth.js
@@ -6,6 +6,9 @@
 
 'use strict';
 
+var AWSValidator = require('./awsvalidator');
+var validator = new AWSValidator();
+
 // Create a function for the Chrome i18n API if it doesn't exist the allow the
 // page to be tested as a normal browser page.
 if (!chrome.i18n) {
@@ -26,6 +29,25 @@ keys.forEach(function(name) {
 
 var button = document.getElementById('mount');
 
+// Add validation patterns to the text fields based on patterns from the
+// validator.
+for (var key in validator.patterns) {
+  fields[key].setAttribute('pattern', validator.patterns[key]);
+}
+
+// Populate the regions datalist with the list from the validator.
+var regionList = document.getElementById('regions');
+
+for (var i = 0; i < validator.regions.length; i++) {
+  var region = validator.regions[i];
+
+  var option = document.createElement('option');
+  option.setAttribute('value', region);
+
+  regionList.appendChild(option);
+}
+
+// Restores previously saved credentials and autofills the text fields.
 var restoreCredentials = function() {
   if (!chrome.storage) { return; }
   chrome.storage.sync.get(keys, function(items) {
@@ -39,6 +61,8 @@ var restoreCredentials = function() {
   });
 };
 
+// Pulls in UI strings for the user's locale and updates the UI elements to
+// display them.
 var internationalise = function() {
   var selector = 'data-message';
   var elements = document.querySelectorAll('[' + selector + ']');
@@ -65,11 +89,39 @@ var internationalise = function() {
   }
 };
 
+var validate = function() {
+    if (!fields.bucket.checkValidity()) {
+    document.getElementById('toast-invalid-bucket').show();
+    return false;
+  }
+
+  // Need to use the validator directly for this one because it's not using a
+  // regular expression like the others.
+  if (!validator.region(fields.region.value)) {
+    document.getElementById('toast-invalid-region').show();
+    return false;
+  }
+
+  // Only show a warning for these, instead of returning false. The format is
+  // not as well defined, so the check is just a guideline, not a rule.
+  if (!fields.access.checkValidity()) {
+    document.getElementById('toast-invalid-access').show();
+  }
+
+  if (!fields.secret.checkValidity()) {
+    document.getElementById('toast-invalid-secret').show();
+  }
+
+  return true;
+};
+
 restoreCredentials();
 internationalise();
 
 button.addEventListener('click', function(event) {
   event.preventDefault();
+
+  if (!validate()) { return; }
 
   button.setAttribute('disabled', 'true');
 
