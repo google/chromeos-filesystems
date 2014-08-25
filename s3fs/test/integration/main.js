8c526321122d64eb041a1ed95f90262557cbf0c6
Giles Lavelle <lavelle@chromium.org>
2014-08-25 11:18:37 -0700

    add more integration tests

--- s3fs/test/integration/main.js
+++ s3fs/test/integration/main.js
@@ -2,12 +2,38 @@ var url = 'http://localhost:9000/build.html';
 
 module.exports = {
   'Page title is correct': function(test) {
-    test.open(url).assert.title().is('mountHeader', 'It has title').done();
+    test.open(url).assert.title('mountHeader', 'It has title').done();
+  },
+
+  'Fields are visible': function(test) {
+    test.assert.chain()
+      .visible('#bucket')
+      .visible('#region')
+      .visible('#access')
+      .visible('#secret')
+    end().done();
   },
 
   'Bucket placeholder is correct': function(test) {
-    test.open(url)
-      .query('#bucket').assert.attr('placeholder', 'bucketName')
+    test.assert.attr('#bucket', 'placeholder', 'bucketName').done();
+  },
+
+  'Region placeholder is correct': function(test) {
+    test.assert.attr('#region', 'placeholder', 'bucketRegion').done();
+  },
+
+  'Access placeholder is correct': function(test) {
+    test.assert.attr('#access', 'placeholder', 'accessKey').done();
+  },
+
+  'Secret placeholder is correct': function(test) {
+    test.assert.attr('#secret', 'placeholder', 'secretKey').done();
+  },
+
+  'Message is shown when attempting to mount bucket': function(test) {
+    test
+      .click('#mount')
+      .assert.visible('#toast-mount-attempt')
       .done();
   }
 };
