fa1876f4d914b2156a7eb00ec116b464ef1b2003
Giles Lavelle <lavelle@chromium.org>
2014-08-25 14:07:13 -0700

    update integration tests for polymer ui

--- s3fs/test/integration/main.js
+++ s3fs/test/integration/main.js
@@ -1,19 +1,15 @@
-<<<<<<< HEAD
 // Copyright 2014 The Chromium Authors. All rights reserved.
 
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file or at
 // https://developers.google.com/open-source/licenses/bsd
 
-'use strict';
+'use strict'
 
-=======
->>>>>>> set up integration testing with dalek.js
 var url = 'http://localhost:9000/build.html';
 
 module.exports = {
   'Page title is correct': function(test) {
-<<<<<<< HEAD
     test.open(url).assert.title('mountHeader', 'It has title').done();
   },
 
@@ -26,34 +22,26 @@ module.exports = {
     end().done();
   },
 
-  'Bucket placeholder is correct': function(test) {
-    test.assert.attr('#bucket', 'placeholder', 'bucketName').done();
+  'Bucket label is correct': function(test) {
+    test.assert.attr('#bucket', 'label', 'bucketName').done();
   },
 
-  'Region placeholder is correct': function(test) {
-    test.assert.attr('#region', 'placeholder', 'bucketRegion').done();
+  'Region label is correct': function(test) {
+    test.assert.attr('#region', 'label', 'bucketRegion').done();
   },
 
-  'Access placeholder is correct': function(test) {
-    test.assert.attr('#access', 'placeholder', 'accessKey').done();
+  'Access label is correct': function(test) {
+    test.assert.attr('#access', 'label', 'accessKey').done();
   },
 
-  'Secret placeholder is correct': function(test) {
-    test.assert.attr('#secret', 'placeholder', 'secretKey').done();
+  'Secret label is correct': function(test) {
+    test.assert.attr('#secret', 'label', 'secretKey').done();
   },
 
   'Message is shown when attempting to mount bucket': function(test) {
     test
       .click('#mount')
       .assert.visible('#toast-mount-attempt')
-=======
-    test.open(url).assert.title().is('mountHeader', 'It has title').done();
-  },
-
-  'Bucket placeholder is correct': function(test) {
-    test.open(url)
-      .query('#bucket').assert.attr('placeholder', 'bucketName')
->>>>>>> set up integration testing with dalek.js
       .done();
   }
 };
