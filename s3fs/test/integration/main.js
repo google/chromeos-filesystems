8da13742b39eaa7c65915e94e9d45823f6e88421
Giles Lavelle <lavelle@chromium.org>
2014-08-25 11:27:52 -0700

    add license

--- s3fs/test/integration/main.js
+++ s3fs/test/integration/main.js
@@ -1,3 +1,11 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+'use strict';
+
 var url = 'http://localhost:9000/build.html';
 
 module.exports = {
