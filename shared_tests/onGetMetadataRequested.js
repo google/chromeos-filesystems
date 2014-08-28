43c25aa3cde2374b4002830c6c1649e574f289b1
Giles Lavelle <lavelle@chromium.org>
2014-08-28 16:53:54 -0700

    missing semicolon

--- shared_tests/onGetMetadataRequested.js
+++ shared_tests/onGetMetadataRequested.js
@@ -19,7 +19,7 @@ module.exports = function(onGetMetadataRequested) {
         metadata.should.have.property('isDirectory', false);
         metadata.should.have.property('name', '1.txt');
         metadata.should.have.property('size', 1);
-        metadata.should.have.property('mimeType')
+        metadata.should.have.property('mimeType');
         metadata.mimeType.should.match(/^text\/plain/);
         metadata.should.have.property('modificationTime')
           .that.is.an.instanceof(Date);
