441b8fbc3a82dc8b6497f4552ab1390297a224c4
Giles Lavelle <lavelle@chromium.org>
2014-09-02 18:10:15 -0700

    move stuff around

--- s3fs/test/functional/main.js
+++ s3fs/test/functional/main.js
@@ -11,22 +11,27 @@ var server = new SeleniumServer('test/selenium.jar', {port: 4444});
 
 server.start();
 
-var driver = new webdriver.Builder()
-  .usingServer(server.address())
-  .withCapabilities(webdriver.Capabilities.firefox())
-  .build();
+var makeDriver = function() {
+  return new webdriver.Builder()
+    .usingServer(server.address())
+    .withCapabilities(webdriver.Capabilities.firefox())
+    .build();
+};
 
-driver.get('http://localhost:3456/build.html');
 // driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
 // driver.findElement(webdriver.By.name('btnG')).click();
 
 describe('title', function() {
   it('should be correct', function(done) {
+    var driver = makeDriver();
+    driver.get('http://localhost:3456/build.html');
+
     driver.getTitle().then(function(title) {
       title.should.equal('mountHeader');
-      // driver.quit();
       done();
     });
+
+    driver.quit();
   });
 });
 
