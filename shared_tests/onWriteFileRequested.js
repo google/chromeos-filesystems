9f46926c9b8a9add2dbfb75f0e976f764cff3731
Giles Lavelle <lavelle@chromium.org>
2014-09-03 11:56:07 -0700

    write test

---
+++ shared_tests/onWriteFileRequested.js
@@ -0,0 +1,15 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file or at
+// https://developers.google.com/open-source/licenses/bsd
+
+'use strict';
+
+module.exports = function(onReadFileRequested, onOpenFileRequested) {
+  describe('onReadFileRequested', function() {
+    it('should write the correct data to a file', function(done) {
+
+    });
+  });
+};
