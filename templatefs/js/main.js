b01cab01a6e10a04b5538d131bcc22e4bda54dfd
Giles Lavelle <lavelle@chromium.org>
2014-09-02 18:18:11 -0700

    add message listener to template

--- templatefs/js/main.js
+++ templatefs/js/main.js
@@ -15,3 +15,32 @@ var events = {
   onReadDirectoryRequested: require('./events/onReadDirectoryRequested')
 };
 
+
+// Main function to handle requests from the settings UI.
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  switch (request.type) {
+    case 'mount':
+      // Mount your instance here using the data from the UI.
+
+      break;
+    default:
+      var message;
+      if (request.type) {
+        message = 'Invalid request type: ' + request.type + '.';
+      } else {
+        message = 'No request type provided.';
+      }
+
+      sendResponse({
+        type: 'error',
+        success: false,
+        message: message
+      });
+      break;
+  }
+
+  // Return true from the event listener to indicate that we will be sending
+  // the response asynchronously, so that the sendResponse function is still
+  // valid at the time it's used.
+  return true;
+});
