5bef89b5578ebffa0d6e26476722f08bfc6627f3
Giles Lavelle <lavelle@chromium.org>
2014-09-03 11:47:06 -0700

    fill out write method

--- s3fs/js/events/onWriteFileRequested.js
+++ s3fs/js/events/onWriteFileRequested.js
@@ -15,5 +15,32 @@
  *     attempting to read the file.
  */
 module.exports = function(options, onSuccess, onError) {
+  if (!(options.openRequestId in s3fs.openedFiles)) {
+    onError('INVALID_OPERATION');
+    return;
+  }
 
+  // Strip the leading slash, since not used internally.
+  var path = s3fs.openedFiles[options.openRequestId].substring(1);
+
+  // Calculate the index of the last byte to fetch. Subtract 1 because HTTP
+  // byte ranges are inclusive.
+  var end = options.offset + options.length - 1;
+
+  var body = options.data.slice(options.offset, end);
+
+  var parameters = s3fs.parameters({
+    Key: path,
+    Body: body,
+    ContentLength: body.byteLength
+  });
+
+  s3fs.s3.putObject(parameters, function(error, data) {
+    if (error) {
+      // TODO(lavelle): add logic for returning more specific error codes.
+      onError('FAILED');
+    } else {
+      onSuccess();
+    }
+  });
 };
