00fbf39bd155647d0f79b87103b6efce1bea4123
Giles Lavelle <giles.lavelle@gmail.com>
2014-09-04 08:34:54 -0700

    fill out first delete test

--- shared_tests/onDeleteEntryRequested.js
+++ shared_tests/onDeleteEntryRequested.js
@@ -6,10 +6,33 @@
 
 'use strict';
 
-module.exports = function(fs, onDeleteEntryRequested) {
+module.exports = function(onDeleteEntryRequested, onGetMetadataRequested) {
   describe('onDeleteEntryRequested', function() {
-    it('should ', function(done) {
-      done();
+    it('should remove an existing file', function(done) {
+      var statOptions = {
+        entryPath: '/1.txt'
+      };
+
+      var deleteOptions = {
+        entryPath: '/1.txt',
+        recursive: false
+      };
+
+      var onError = function(error) {
+        throw new Error(error);
+      };
+
+      onGetMetadataRequested(statOptions, function() {
+        onDeleteEntryRequested(deleteOptions, function() {
+          onGetMetadataRequested(statOptions, function() {
+            throw new Error('Delete operation failed to remove file.');
+          }, function(error) {
+            error.should.be.a('string');
+            error.should.equal('NOT_FOUND');
+            done();
+          });
+        }, onError);
+      }, onError);
     });
   });
 };
