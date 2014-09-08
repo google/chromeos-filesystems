3161c230d0b5ce6625e5187dd4aad220901a4843
Giles Lavelle <lavelle@chromium.org>
2014-09-08 14:34:56 -0700

    remove duplication in truncate test

--- shared_tests/onTruncateFileRequested.js
+++ shared_tests/onTruncateFileRequested.js
@@ -8,149 +8,75 @@
 
 var util = require('../shared/util');
 
+var testFactory = function(open, read, truncate) {
+  return function(file, id, length, expected) {
+    return function(done) {
+      var openOptions = {
+        filePath: file,
+        mode: 'WRITE',
+        create: false,
+        requestId: id
+      };
+
+      var readOptions = {
+        length: 512,
+        offset: 0,
+        openRequestId: id
+      };
+
+      var truncateOptions = {
+        filePath: file,
+        length: length
+      };
+
+      var onError = function(error) {
+        throw new Error(error);
+      };
+
+      open(openOptions, function() {
+        read(readOptions, function(data) {
+          var before = util.arrayBufferToString(data);
+          before.should.have.length(expected.before.length);
+          before.should.equal(expected.before);
+
+          truncate(truncateOptions, function() {
+            read(readOptions, function(data) {
+              var after = util.arrayBufferToString(data);
+              after.should.have.length(expected.after.length);
+              after.should.equal(expected.after);
+
+              done();
+            }, onError);
+          }, onError);
+        }, onError);
+      }, onError);
+    };
+  };
+};
+
 module.exports = function(onTruncateFileRequested, onReadFileRequested,
   onOpenFileRequested) {
     describe('onTruncateFileRequested', function() {
+      var testTruncate = testFactory(onOpenFileRequested,
+        onReadFileRequested, onTruncateFileRequested);
+
       it('should truncate the contents of a file to the correct length',
-        function(done) {
-          var id = 999;
-          var file = '/truncatable.txt';
+        testTruncate('/truncatable.txt', 999, 10, {
+          before: 'abcdefghijklmnopqrstuvwxyz',
+          after: 'abcdefghij'
+        }));
 
-          var openOptions = {
-            filePath: file,
-            mode: 'WRITE',
-            create: false,
-            requestId: id
-          };
-
-          var readOptions = {
-            length: 512,
-            offset: 0,
-            openRequestId: id
-          };
-
-          var truncateOptions = {
-            filePath: file,
-            length: 10
-          };
-
-          var onError = function(error) {
-            throw new Error(error);
-          };
-
-          onOpenFileRequested(openOptions, function() {
-            onReadFileRequested(readOptions, function(data) {
-              var before = util.arrayBufferToString(data);
-              before.should.have.length(26);
-              before.should.equal('abcdefghijklmnopqrstuvwxyz');
-
-              onTruncateFileRequested(truncateOptions, function() {
-                onReadFileRequested(readOptions, function(data) {
-                  var after = util.arrayBufferToString(data);
-                  after.should.have.length(10);
-                  after.should.equal('abcdefghij');
-
-                  done();
-                }, onError);
-              }, onError);
-            }, onError);
-          }, onError);
-      });
-    });
-
-    describe('onTruncateFileRequested', function() {
       it('should truncate the contents of a file to length zero correctly',
-        function(done) {
-          var id = 998;
-          var file = '/2.txt';
+        testTruncate('/2.txt', 998, 0, {
+          before: '2',
+          after: ''
+        }));
 
-          var openOptions = {
-            filePath: file,
-            mode: 'WRITE',
-            create: false,
-            requestId: id
-          };
-
-          var readOptions = {
-            length: 512,
-            offset: 0,
-            openRequestId: id
-          };
-
-          var truncateOptions = {
-            filePath: file,
-            length: 0
-          };
-
-          var onError = function(error) {
-            throw new Error(error);
-          };
-
-          onOpenFileRequested(openOptions, function() {
-            onReadFileRequested(readOptions, function(data) {
-              var before = util.arrayBufferToString(data);
-              before.should.have.length(1);
-              before.should.equal('2');
-
-              onTruncateFileRequested(truncateOptions, function() {
-                onReadFileRequested(readOptions, function(data) {
-                  var after = util.arrayBufferToString(data);
-                  after.should.have.length(0);
-                  after.should.equal('');
-
-                  done();
-                }, onError);
-              }, onError);
-            }, onError);
-          }, onError);
-      });
-    });
-
-    describe('onTruncateFileRequested', function() {
       it('should pad a file with null bytes when truncating with a length ' +
-        'longer than that of the original file', function(done) {
-          var id = 997;
-          var file = '/11.txt';
-
-          var openOptions = {
-            filePath: file,
-            mode: 'WRITE',
-            create: false,
-            requestId: id
-          };
-
-          var readOptions = {
-            length: 512,
-            offset: 0,
-            openRequestId: id
-          };
-
-          var truncateOptions = {
-            filePath: file,
-            length: 10
-          };
-
-          var onError = function(error) {
-            throw new Error(error);
-          };
-
-          onOpenFileRequested(openOptions, function() {
-            onReadFileRequested(readOptions, function(data) {
-              var before = util.arrayBufferToString(data);
-              before.should.have.length(2);
-              before.should.equal('11');
-
-              onTruncateFileRequested(truncateOptions, function() {
-                onReadFileRequested(readOptions, function(data) {
-                  var after = util.arrayBufferToString(data);
-                  after.should.have.length(10);
-                  after.should.equal('11\0\0\0\0\0\0\0\0');
-
-                  done();
-                }, onError);
-              }, onError);
-            }, onError);
-          }, onError);
-      });
+        'longer than that of the original file',
+        testTruncate('/11.txt', 997, 10, {
+          before: '11',
+          after: '11\0\0\0\0\0\0\0\0'
+        }));
     });
 };
